6276bdebf0cfceb26b5e6bd5b9e3645862fc5dc1
Alec German <kimarimt@protonmail.com>
2025-06-01 11:46:24 -0400

    finished exercise 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,38 +1,29 @@
-import Content from './Content'
-import Header from './Header'
-import Total from './Total'
+import Course from './Course'
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
+        id: 1,
         name: 'Fundamentals of React',
         exercises: 10
       },
       {
+        id: 2,
         name: 'Using props to pass data',
         exercises: 7
       },
       {
+        id: 3,
         name: 'State of a component',
         exercises: 14
       }
     ]
   }
 
-  const totalExercises = course.parts.reduce(
-    (prev, curr) => prev + curr.exercises,
-    0
-  )
-
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total totalExercises={totalExercises} />
-    </>
-  )
+  return <Course course={course} />
 }
 
 export default App
