a09fd3bb722a89303d79cf633d5f746f170f153e
Alec German <kimarimt@protonmail.com>
2025-06-01 21:09:26 -0400

    finished exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,6 +1,52 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [name, setName] = useState('')
+  const [contacts, setContacts] = useState([
+    { id: 1, name: 'Arto Hellas' }
+  ])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    const newContact = {
+      id: contacts.length + 1,
+      name
+    }
+
+    setContacts(contacts.concat(newContact))
+    setName('')
+  }
+
   return (
-    <h1>Hello, World!</h1>
+    <>
+      <div>
+        <h2>Phonebook</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>name: </label>
+            <input 
+              id='name'
+              name='name'
+              onChange={({ target }) => setName(target.value)}
+              type='text' 
+              value={name}
+            />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2>Numbers</h2>
+        {
+          contacts.map(contact =>
+            <p key={contact.id}>{contact.name}</p>
+          )
+        }
+      </div>
+    </>
   )
 }
 
