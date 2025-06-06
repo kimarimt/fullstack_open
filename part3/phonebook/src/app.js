eec6a1dc5683571219f65c444900a4bf8b32ee46
Alec German <kimarimt@protonmail.com>
2025-06-06 02:13:51 -0400

    finished exercise 3.1

--- part3/phonebook/src/app.js
+++ part3/phonebook/src/app.js
@@ -1,11 +1,10 @@
 import express from 'express'
+import contactsRouter from './controllers/contacts.js'
 
 const port = 3001
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send("<h1>Hello, World</h1>")
-})
+app.use('/api/contacts', contactsRouter)
 
 app.listen(port, () => {
   console.log(`[server]: Listening at http://localhost:${port}`)
---
+++ part3/phonebook/src/controllers/contacts.js
@@ -0,0 +1,32 @@
+import express from 'express'
+
+const router = express.Router()
+
+const contacts = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "phoneNumber": "1-923-668-9296"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "phoneNumber": "638-658-9605"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "phoneNumber": "1-928-311-5358"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "phoneNumber": "335-230-3499"
+    }
+]
+
+router.get('/', (req, res) => {
+  res.json(contacts)
+})
+
+export default router
