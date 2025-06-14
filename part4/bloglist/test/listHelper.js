50d13be0a160bf19f73ee4a392f0a0332f8653a7
Alec German <kimarimt@protonmail.com>
2025-06-14 11:57:03 -0400

    finished exercise 4.4

--- part4/bloglist/test/listHelper.js
+++ part4/bloglist/test/listHelper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs
+    .reduce((prev, curr) => prev + curr.likes, 0)
+}
+
 export default {
   dummy,
+  totalLikes,
 }
--- part4/bloglist/test/listHelper.test.js
+++ part4/bloglist/test/listHelper.test.js
@@ -8,3 +8,40 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
 })
+
+describe('total likes', () => {
+  const blogList = [
+    {
+      _id: '684d993f9bf26703a98d62cf',
+      title: '[ On | No ] syntactic support for error handling',
+      author: 'Robert Griesemer',
+      url: 'https://go.dev/blog/error-syntax',
+      likes: 10,
+    },
+    {
+      _id: '684d99e24bd7ef2ed72d3035',
+      title: 'More predictable benchmarking with testing.B.Loop',
+      author: 'Junyang Shao',
+      url: 'https://go.dev/blog/testing-b-loop',
+      likes: 5,
+    },
+    {
+      _id: '684d9a8c790945fedc924731',
+      title: 'Traversal-resistant file APIs',
+      author: 'Damien Neil',
+      url: 'https://go.dev/blog/osroot',
+      likes: 7,
+    },
+  ]
+
+  test('when list has only one blog, total likes is equal to the blogs likes', () => {
+    const blogs = blogList.slice(0, 1)
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, blogs[0].likes)
+  })
+
+  test('returns the total likes of list of blogs', () => {
+    const result = listHelper.totalLikes(blogList)
+    assert.strictEqual(result, 22)
+  })
+})
