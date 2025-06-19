90a3f44657b7e598ef0e79f47d8e15658f67657d
Alec German <kimarimt@protonmail.com>
2025-06-19 03:44:00 -0400

    fininshed exercise 4.18

--- part4/bloglist/src/app.js
+++ part4/bloglist/src/app.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose'
 import morgan from 'morgan'
 import config from './utils/config.js'
 import middleware from './utils/middleware.js'
-import blogsRouter from './controllers/blog.js'
+import loginRouter from './controllers/login.js'
+import blogRouter from './controllers/blog.js'
 import userRouter from './controllers/user.js'
 
 const app = express()
@@ -26,8 +27,9 @@ morgan.token('body', (req, res) => {
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use('/api/login', loginRouter)
 app.use('/api/users', userRouter)
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', blogRouter)
 app.use(middleware.errorHandler)
 app.use(middleware.unknownEndpoint)
 
---
+++ part4/bloglist/src/controllers/login.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import bcrypt from 'bcrypt'
+import dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
+import User from '../models/user.js'
+
+dotenv.config()
+
+const router = express.Router()
+
+router.post('/', async (req, res) => {
+  const { username, password } = req.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : bcrypt.compareSync(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).send({
+      error: 'invalid username or password',
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  res.json({
+    token,
+    username: user.username,
+    name: user.name,
+  })
+})
+
+export default router
