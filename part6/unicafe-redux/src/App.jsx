838e34b0c6136f1957e0e155420d5698849f96e1
Alec German <kimarimt@protonmail.com>
2025-06-28 06:05:49 -0400

    finished exercise 6.2

--- part6/unicafe-redux/src/App.jsx
+++ part6/unicafe-redux/src/App.jsx
@@ -1,6 +1,36 @@
-const App = () => {
+const App = ({ store }) => {
+  const state = store.getState()
+
   return (
-    <h1>Hello, World!</h1>
+    <>
+      <div>
+        <button 
+          onClick={() => store.dispatch({ type: 'GOOD' })}
+        >
+          good
+        </button>
+        <button 
+          onClick={() => store.dispatch({ type: 'OK' })}
+        >
+          ok
+        </button>
+        <button 
+          onClick={() => store.dispatch({ type: 'BAD' })}
+        >
+          bad
+        </button>
+        <button 
+          onClick={() => store.dispatch({ type: 'ZERO' })}
+        >
+          reset stats
+        </button>
+      </div>
+      <p>
+        good {state.good}<br />
+        ok {state.ok}<br />
+        bad {state.bad}
+      </p>
+    </>
   )
 }
 
--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createStore } from 'redux'
+import counterReducer from './reducers/counterReducer'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const store = createStore(counterReducer)
+const root = createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(
+    <StrictMode>
+      <App store={store} />
+    </StrictMode>
+  )
+}
+
+renderApp()
+store.subscribe(renderApp)
