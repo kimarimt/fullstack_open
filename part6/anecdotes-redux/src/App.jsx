5a97f333fec4bf759c6962ecbb08d701d0f1f27f
Alec German <kimarimt@protonmail.com>
2025-06-29 04:00:30 -0400

    finished exercise 6.3

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,6 +1,22 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { addVote } from './reducers/anecdoteReducer'
+
 const App = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state.anecdotes)
+
   return (
-    <h1>Hello, World!</h1>
+    <>
+      <h1>Redux Anecdotes</h1>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <p key={anecdote.id}>
+          {anecdote.text}<br />
+          has {anecdote.votes} votes. 
+          <button onClick={() => dispatch(addVote(anecdote.id))}>vote</button>
+        </p>
+      )}
+    </>
   )
 }
 
--- part6/anecdotes-redux/src/main.jsx
+++ part6/anecdotes-redux/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import App from './App'
+
+const store = createStore(anecdoteReducer)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
