88b8d48ba0bba818e542e06c105b1fe8ed9846c7
Alec German <kimarimt@protonmail.com>
2025-07-01 10:37:21 -0400

    finished exercise 6.20

--- part6/anecdotes-query/src/App.jsx
+++ part6/anecdotes-query/src/App.jsx
@@ -1,34 +1,41 @@
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './services/anecdotes'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
-  const anecdotes = [
-    {
-      'text': 'If it hurts, do it more often',
-      'id': '47145',
-      'votes': 0
-    }
-  ]
-
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes
+  })
+  
   const handleVote = (anecdote) => {
     console.log(`you voted for ${anecdote.text}`)
   }
+  
+  const anecdotes = data
 
   return (
-    <div>
-      <h1>Anecdote App</h1>
-      <Notification />
-      <AnecdoteForm />
-      {anecdotes.map(anecdote => (
-        <div key={anecdote.id}>
-          <p>{anecdote.text}</p>
-          <p>
-            has {anecdote.votes}
-            <button onClick={() => handleVote(anecdote)}>vote</button>
-          </p>
+    <>
+      {isLoading && <p>loading data...</p>}
+      {isError && <p>anecdote service not available due to problems in the server</p>}
+      {anecdotes && (
+        <div>
+          <h1>Anecdote App</h1>
+          {/* <Notification /> */}
+          <AnecdoteForm />
+          {anecdotes.map(anecdote => (
+            <div key={anecdote.id}>
+              <p>{anecdote.text}</p>
+              <p>
+                has {anecdote.votes}
+                <button onClick={() => handleVote(anecdote)}>vote</button>
+              </p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   )
 }
 
