a767ddf87e476f6aee496173ae28b6fb57de7b07
Alec German <kimarimt@protonmail.com>
2025-09-17 23:33:45 -0400

    courseinfo part 2 setup

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,11 @@
+import Part from './Part'
+
+const Content = ({ parts }) => (
+  <div>
+    {
+      parts.map((part, i) => <Part key={i} part={part} />)
+    }
+  </div>
+)
+
+export default Content
