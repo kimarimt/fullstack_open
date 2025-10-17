d58b8c825a534409be3ee2f22c78c15638298689
Alec German <kimarimt@protonmail.com>
2025-10-17 18:02:44 -0400

    finished exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,7 @@
+import Content from './components/Content'
+import Header from './components/Header'
+import Total from './components/Total'
+
 export default function App() {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +13,20 @@ export default function App() {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercise1}
-      </p>
-      <p>
-        {part2} {exercise2}
-      </p>
-      <p>
-        {part3} {exercise3}
-      </p>
-      <p>Number of exercises {exercise1 + exercise2 + exercise3}</p>
+      <Header course={course} />
+      <Content 
+        part1={part1}
+        exercise1={exercise1}
+        part2={part2}
+        exercise2={exercise2}
+        part3={part3}
+        exercise3={exercise3}
+      />
+      <Total 
+        exercise1={exercise1}
+        exercise2={exercise2}
+        exercise3={exercise3}
+      />
     </div>
   )
 }
---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,15 @@
+export default function Content({ part1, exercise1, part2, exercise2, part3, exercise3 }) {
+  return (
+    <>
+      <p>
+        {part1} {exercise1}
+      </p>
+      <p>
+        {part2} {exercise2}
+      </p>
+      <p>
+        {part3} {exercise3}
+      </p>
+    </>
+  )
+}
