d7f19acc4f3de0145590bbfa1facdb3940750eb3
Alec German <kimarimt@protonmail.com>
2025-10-19 17:58:11 -0400

    finished exercises 2.4-2.5

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -3,7 +3,9 @@ import Part from './Part';
 export default function Content({ parts }) {
   return (
     <>
-      {parts.map(part => <Part key={part.id} part={part} />)}
+      {parts.map(part => 
+        <Part key={part.id} part={part} />)
+      }
     </>
   )
 }
