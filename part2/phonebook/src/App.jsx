17bc44a9b2f8ec20b45a8a80d8d17f49283b6e74
Alec German <kimarimt@protonmail.com>
2025-10-20 17:38:30 -0400

    finished exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import contactService from './services/contact'
 import Filter from './components/Filter'
 import NewContactForm from './components/NewContactForm'
 import Contacts from './components/Contacts'
 
 export default function App() {
-  const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', phoneNumber: '1-978-754-6980' },
-    { name: 'Ada Lovelace', phoneNumber: '1-256-733-2241' },
-    { name: 'Dan Abramov', phoneNumber: '1-564-869-6404' },
-    { name: 'Mary Poppendieck', phoneNumber: '1-305-277-6659' }
-  ])
-  
+  const [contacts, setContacts] = useState(null)
   const [search, setSearch] = useState('')
-  const matches = contacts.filter(contact => contact.name.includes(search))
+  
+  const matches = contacts 
+    ? contacts.filter(contact => contact.name.includes(search))
+    : null
+
+  useEffect(function () {
+    async function fetchContacts() {
+      const contactsData = await contactService.getAll()
+      setContacts(contactsData)
+    }
+
+    fetchContacts()
+  }, [])
 
   function addContact({ name, phoneNumber }) {
     if (contacts.find(contact => contact.name === name)) {
@@ -24,17 +31,21 @@ export default function App() {
     return true
   }
 
-  return (
-    <div>
-      <h1>Phonebook</h1>
-      <Filter 
-        search={search} 
-        onChange={({ target }) => setSearch(target.value)} 
-      />
-      <h2>New Contact</h2>
-      <NewContactForm onSubmit={addContact} />
-      <h2>Contacts</h2>
-      <Contacts contacts={matches} />
-    </div>
+  return ( 
+    <>
+      {matches && (
+        <div>
+          <h1>Phonebook</h1>
+          <Filter 
+            search={search} 
+            onChange={({ target }) => setSearch(target.value)} 
+          />
+          <h2>New Contact</h2>
+          <NewContactForm onSubmit={addContact} />
+          <h2>Contacts</h2>
+          <Contacts contacts={matches} />
+        </div>
+      )}
+    </>
   )
 }
