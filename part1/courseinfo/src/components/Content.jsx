a966608c9a24acd5aa44e2e2394927960fb9a7ab
Alec German <kimarimt@protonmail.com>
2025-11-17 16:43:15 -0500

    finished exercise 1.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,14 +1,10 @@
+import Part from './Part'
+
 const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => (
   <>
-    <p>
-      {part1} {exercises1}
-    </p>
-    <p>
-      {part2} {exercises2}
-    </p>
-    <p>
-      {part3} {exercises3}
-    </p>
+    <Part name={part1} exercises={exercises1} />
+    <Part name={part2} exercises={exercises2} />
+    <Part name={part3} exercises={exercises3} />
   </>
 )
 
