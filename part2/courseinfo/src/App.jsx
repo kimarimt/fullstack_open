edc748b56c19ef07831d72a92ab3864cc5172b70
Alec German <kimarimt@protonmail.com>
2025-11-20 12:19:07 -0500

    part 2 courseinfo project setup

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,37 @@
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div> 
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
