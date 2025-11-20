3d31bb10352c809c44bdb54fcda0a880b34b9d29
Alec German <kimarimt@protonmail.com>
2025-11-20 12:25:12 -0500

    finished exercise 2.2

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,6 +1,8 @@
 const Total = ({ parts }) => 
   <p>
-    Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}
+    <strong>
+      total of {parts[0].exercises + parts[1].exercises + parts[2].exercises} exercises
+    </strong>
   </p>
 
 export default Total
