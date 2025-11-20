f30330a9a833bc98f665c825f0c0ec5a24060f2a
Alec German <kimarimt@protonmail.com>
2025-11-20 12:32:59 -0500

    finished exercises 2.4-2.5

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -2,10 +2,9 @@ import Part from './Part'
 
 const Content = ({ parts }) => (
   <>
-    <Part part={parts[0]} />
-    <Part part={parts[1]} />
-    <Part part={parts[2]} />
-    <Part part={parts[3]} />
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
   </>
 )
 
