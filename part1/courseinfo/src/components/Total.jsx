2b6aa5d46223a7f9404ab3f15de80adcc94b4c63
Alec German <kimarimt@protonmail.com>
2025-11-28 17:50:41 -0500

    finished exercise 1.1

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,7 @@
+const Total = props => {
+  const totalExercises = props.exercises1 + props.exercises2 + props.exercises3
+
+  return <p>Number of exercises {totalExercises}</p>
+}
+
+export default Total
