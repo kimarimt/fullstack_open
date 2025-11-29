8c4086dafa3479e624dd911d079bd881b3cfb560
Alec German <kimarimt@protonmail.com>
2025-11-29 14:23:15 -0500

    finished exercises 2.2-2.3

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,7 +1,11 @@
 const Total = ({ parts }) => {
-  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  const totalExercises = parts.reduce((prev, curr) => prev + curr.exercises, 0)
 
-  return <p>Number of exercises {totalExercises}</p>
+  return (
+    <p>
+      <strong>total of {totalExercises} exercises</strong>
+    </p>
+  )
 }
 
 export default Total
