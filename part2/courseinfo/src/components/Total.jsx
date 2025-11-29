2280f85cea3003f0180f5048f76abbcc952afc4a
Alec German <kimarimt@protonmail.com>
2025-11-29 14:14:06 -0500

    part2 courseinfo project setup

---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,7 @@
+const Total = ({ parts }) => {
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises
+
+  return <p>Number of exercises {totalExercises}</p>
+}
+
+export default Total
