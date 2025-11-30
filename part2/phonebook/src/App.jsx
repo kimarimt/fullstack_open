2e6f6a2fb03b3c5aa3e7fe75e21d58fec2804fa8
Alec German <kimarimt@protonmail.com>
2025-11-30 14:35:16 -0500

    finished exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phoneNumber: '505-396-2710' }
   ])
  
   const [name, setName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -17,11 +18,13 @@ const App = () => {
     }
 
     const newContact = {
-      name
+      name,
+      phoneNumber
     }
 
     setContacts(contacts.concat(newContact))
     setName('')
+    setPhoneNumber('')
   }
 
   return (
@@ -38,12 +41,21 @@ const App = () => {
           />
         </div>
         <div>
+          <label htmlFor='phoneNumber'>Phone Number: </label>
+          <input 
+            id='phoneNumber'
+            type='text' 
+            value={phoneNumber}
+            onChange={({ target }) => setPhoneNumber(target.value)}
+          />
+        </div>
+        <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {contacts.map(contact => 
-        <p key={contact.name}>{contact.name}</p>
+        <p key={contact.name}>{contact.name} {contact.phoneNumber}</p>
       )}
     </div>
   )
