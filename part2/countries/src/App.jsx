ea9c3f9b8365ed6ac169f297f5138c30952632c4
Alec German <kimarimt@protonmail.com>
2025-12-04 12:39:06 -0500

    finished exercise 2.18

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,8 +1,49 @@
+import { useEffect, useState } from 'react'
+import CountryDetails from './components/CountryDetails'
+import countryService from './services/country'
+
 const App = () => {
+  const [countries, setCountres] = useState(null)
+  const [search, setSearch] = useState('')
+
+  const searchResults = countries
+    ? countries.filter(country => country.name.common.includes(search))
+    : null
+
+  useEffect(() => {
+    const fetchCountries = async () => {
+      const countriesData = await countryService.getCountries()
+      setCountres(countriesData)
+    }
+
+    fetchCountries()
+  }, [])
+
   return (
-    <div>
-      <h1>Hello, World</h1>
-    </div>
+    <>
+      {searchResults && (
+        <div>
+          <div>
+            <label htmlFor='search'>Search countries: </label>
+            <input 
+              id='search'
+              type='text'
+              value={search}
+              onChange={({ target }) => setSearch(target.value)}
+            />
+          </div>
+          <div>
+            {searchResults.length > 10 && 
+              <p>Too many matches, specify another filter</p> 
+            }
+            {searchResults.length > 1 && searchResults.length <= 10 && (searchResults.map(country => 
+              <p key={country.cca2}>{country.name.common}</p>
+            ))}
+            {searchResults.length === 1 && <CountryDetails country={searchResults[0]} />}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
---
+++ part2/countries/src/services/country.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all'
+
+const getCountries = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+export default {
+  getCountries
+}
