3a4ff4c40716d8c7ea9e12bb6c8484335feaf5f5
Alec German <kimarimt@protonmail.com>
2025-12-12 15:44:27 -0500

    finished exercise 3.1

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -1,9 +1,32 @@
 import express from 'express'
 
+let contacts = [
+  {
+    "id": "1",
+    "name": "Arto Hellas",
+    "phoneNumber": "206-771-0040",
+  },
+  {
+    "id": "2",
+    "name": "Ada Lovelace",
+    "phoneNumber": "505-646-2892",
+  },
+  {
+    "id": "3",
+    "name": "Dan Abramov",
+    "phoneNumber": "305-202-6061",
+  },
+  {
+    "id": "4",
+    "name": "Mary Poppendieck",
+    "phoneNumber": "331-955-3424",
+  },
+]
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello, World!</h1>')
+app.get('/api/contacts', (req, res) => {
+  res.json(contacts)
 })
 
 const port = 3001
