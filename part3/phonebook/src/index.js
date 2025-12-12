59bbc1054854483fc1e877ab8c4f32878564604b
Alec German <kimarimt@protonmail.com>
2025-12-12 16:55:36 -0500

    edited src/index.js

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -2,24 +2,24 @@ import express from 'express'
 
 let contacts = [
   {
-    "id": "1",
-    "name": "Arto Hellas",
-    "phoneNumber": "206-771-0040",
+    'id': '1',
+    'name': 'Arto Hellas',
+    'phoneNumber': '206-771-0040',
   },
   {
-    "id": "2",
-    "name": "Ada Lovelace",
-    "phoneNumber": "505-646-2892",
+    'id': '2',
+    'name': 'Ada Lovelace',
+    'phoneNumber': '505-646-2892',
   },
   {
-    "id": "3",
-    "name": "Dan Abramov",
-    "phoneNumber": "305-202-6061",
+    'id': '3',
+    'name': 'Dan Abramov',
+    'phoneNumber': '305-202-6061',
   },
   {
-    "id": "4",
-    "name": "Mary Poppendieck",
-    "phoneNumber": "331-955-3424",
+    'id': '4',
+    'name': 'Mary Poppendieck',
+    'phoneNumber': '331-955-3424',
   },
 ]
 
