d4071d52010d9812f76aaef2a84f2d6469486928
Alec German <kimarimt@protonmail.com>
2026-07-10 12:22:30 -0400

    finished exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,31 +4,33 @@ import Total from './components/Total'
 
 const App = () => {
   const course = 'Half Stack Application Developement'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ]
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
+        part1={parts[0]}
+        part2={parts[1]}
+        part3={parts[2]}
       />  
       <Total 
-        exercise1={part1.exercises}
-        exercise2={part2.exercises}
-        exercise3={part3.exercises}
+        exercise1={parts[0].exercises}
+        exercise2={parts[1].exercises}
+        exercise3={parts[2].exercises}
       />
     </div>
   )
