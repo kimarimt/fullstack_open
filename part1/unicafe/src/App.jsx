269c41c646390bdd85ca615c2899faaf49548a2b
Alec German <kimarimt@protonmail.com>
2026-07-10 13:04:33 -0400

    finished exercise 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,24 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <div>
+        <h2>give feedback</h2>
+        <div>
+          <button onClick={() => setGood(good + 1)}>good</button>
+          <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+          <button onClick={() => setBad(bad + 1)}>bad</button>
+        </div>
+      </div>
+      <div>
+        <h2>statistics</h2>
+        <p>
+          good {good}<br />
+          neutral {neutral} <br />
+          bad {bad}
+        </p>
+      </div>
+    </>
   )
 }
 
