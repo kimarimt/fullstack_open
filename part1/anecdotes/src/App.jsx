9156c0a89a241457aa5cfe9b7891ca65010f935f
Alec German <kimarimt@protonmail.com>
2026-08-23 00:01:23 -0400

    finished exercise 1.12

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -14,9 +14,15 @@ const App = () => {
 
   const [selected, setSelected] = useState(0)
 
+  function selectAnecdote() {
+    const n = Math.floor(Math.random() * anecdotes.length)
+    setSelected(n)
+  }
+
   return (
     <>
       <p>{anecdotes[selected]}</p>
+      <button onClick={selectAnecdote}>next</button>
     </>
   )
 }
