72e7dd635b531eb34b01c6af9e5facd79d799e14
Alec German <kimarimt@protonmail.com>
2026-08-24 06:17:26 -0400

    finished exercise 2.6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,20 +6,34 @@ const App = () => {
   ])
   const [name, setName] = useState('')
 
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    setContacts([...contacts, { name }])
+    setName('')
+  }
+
   return (
     <>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Name: </label>
           <input 
             id='name'
             name='name'
             type='text'
+            value={name}
+            onChange={handleChange}
           />
         </div>
         <div>
-          <button type='submit'>Add</button>
+          <button type='submit'>
+            Add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
