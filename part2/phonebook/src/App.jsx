288aaf7635610eba3a53422819040875ff73b66f
Alec German <kimarimt@protonmail.com>
2026-09-10 05:17:16 -0400

    finished exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    { name: 'Arto Hellas', phoneNumber: '305-204-3201' }
+    { id: 1, name: 'Arto Hellas', phoneNumber: '305-204-3201' },
+    { id: 2, name: 'Ada Lovelace', phoneNumber: '305-200-7808' },
+    { id: 3, name: 'Dan Abramov', phoneNumber: '762-900-9760' },
+    { id: 4, name: 'Mary Poppendieck', phoneNumber: '305-863-3750' },
   ])
+
+  const [searchTerm, setSearchTerm] = useState('')
   const [name, setName] = useState('')
   const [phoneNumber, setPhoneNumber] = useState('')
+
+  const handleTermChange = event => {
+    setSearchTerm(event.target.value)
+  }
 
   const handleNameChange = event => {
     setName(event.target.value)
@@ -29,40 +38,57 @@ const App = () => {
     }
   }
 
+  const filteredContacts = searchTerm
+    ? contacts.filter(contact => contact.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    : contacts
+
   return (
     <>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='name'>Name: </label>
-          <input 
-            id='name'
-            name='name'
-            type='text'
-            value={name}
-            onChange={handleNameChange}
-          />
-        </div>
-        <div>
-          <label htmlFor='phoneNumber'>Number: </label>
-          <input 
-            id='phoneNumber' 
-            name='phoneNumber' 
-            type='text'
-            value={phoneNumber}
-            onChange={handlePhoneNumberChange}
-          />
-        </div>
-        <div>
-          <button type='submit'>
-            Add
-          </button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {contacts.map((contact) => 
-        <p key={contact.name}>{contact.name} {contact.phoneNumber}</p>
-      )}
+      <h1>Phonebook</h1>
+      <div>
+        <label>Search Contacts: </label>
+        <input 
+          type='text'
+          value={searchTerm}
+          onChange={handleTermChange}
+        />
+      </div>
+      <div>
+        <h2>New Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor='name'>Name: </label>
+            <input 
+              id='name'
+              name='name'
+              type='text'
+              value={name}
+              onChange={handleNameChange}
+            />
+          </div>
+          <div>
+            <label htmlFor='phoneNumber'>Number: </label>
+            <input 
+              id='phoneNumber' 
+              name='phoneNumber' 
+              type='text'
+              value={phoneNumber}
+              onChange={handlePhoneNumberChange}
+            />
+          </div>
+          <div>
+            <button type='submit'>
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2>Contacts</h2>
+        {filteredContacts.map((contact) => 
+          <p key={contact.id}>{contact.name} {contact.phoneNumber}</p>
+        )}
+      </div>
     </>
   )
 }
